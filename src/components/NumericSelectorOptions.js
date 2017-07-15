4e0f146ddce4e4070b139c4f5da12644a5e5410e
Anurag Kapur <anurag@beancrunch.com>
2017-07-15 23:57:46 +0100

    Add inital find-a-sub form that submits to server

--- src/components/NumericSelectorOptions.js
+++ src/components/NumericSelectorOptions.js
@@ -5,10 +5,10 @@ export default class NumericSelectorOptions extends React.Component {
   render() {
     let rows = []
     for (var i = this.props.start; i < parseInt(this.props.start)+parseInt(this.props.count); i++) {
-      rows.push(<option key={i}>{i}</option>)
+      rows.push(<option key={i} name="number" id={i}>{i}</option>)
     }
     return (
-      <select className="form-control">
+      <select className="form-control" name="number" id="number">
         {rows}
       </select>
     )
--- src/pages/NewRequest.js
+++ src/pages/NewRequest.js
@@ -8,47 +8,47 @@ class NewRequest extends React.Component {
     return (
       <div>
         <Nav />
-        <div className="below-nav-div">
-          <form className="form-horizontal">
+        <div className="below-nav-div container">
+          <form className="form-horizontal transparent-panel" method="POST" action="https://oxsub-api.herokuapp.com/find-a-sub">
             <div className="form-group">
               <label className="col-sm-2 col-sm-offset-2 control-label">I am looking for</label>
               <div className="col-sm-5">
                 <label className="radio-inline">
-                  <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />Stroke side rower
+                  <input type="radio" name="subType" id="inlineRadio1" value="COX" checked/>Any Cox
                 </label>
                 <label className="radio-inline">
-                  <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />Bow side rower
+                  <input type="radio" name="subType" id="inlineRadio1" value="XSTATUSCOX"/>X Status Cox
                 </label>
                 <label className="radio-inline">
-                  <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />Any side rower
+                  <input type="radio" name="subType" id="inlineRadio1" value="SSTATUSCOX"/>S Status Cox
                 </label>
                 <br />
-                <label className="radio-inline">
-                  <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />X-status Cox
+                <label className="radio-inline active">
+                  <input type="radio" name="subType" id="inlineRadio1" value="ROWER"/>Any Side Rower
                 </label>
                 <label className="radio-inline">
-                  <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />S-status Cox
+                  <input type="radio" name="subType" id="inlineRadio1" value="STOKESIDEROWER"/>Stroke Side Rower
                 </label>
                 <label className="radio-inline">
-                  <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />Any-status Cox
+                  <input type="radio" name="subType" id="inlineRadio1" value="BOWSIDEROWER"/>Bow Side Rower
                 </label>
               </div>
             </div>
             <div className="form-group">
               <label className="col-sm-2 col-sm-offset-2 control-label">How many?</label>
               <div className="col-sm-5">
-                <NumericSelectorOptions start="1" count="8" />
+                <NumericSelectorOptions start="1" count="8"/>
               </div>
             </div>
             <div className="form-group">
-              <label className="col-sm-2 col-sm-offset-2 control-label">Password</label>
+              <label className="col-sm-2 col-sm-offset-2 control-label">For crew</label>
               <div className="col-sm-5">
-                <input type="password" className="form-control" id="inputPassword3" placeholder="Password" />
+                <input type="text" name="crew" className="form-control" id="crew" placeholder="Christ Church M1"/>
               </div>
             </div>
             <div className="form-group">
               <div className="col-sm-offset-2 col-sm-10">
-                <button type="submit" className="btn btn-default">Sign in</button>
+                <button type="submit" className="btn btn-default">Submit</button>
               </div>
             </div>
           </form>
