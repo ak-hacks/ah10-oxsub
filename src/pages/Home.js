52165a81b2e8d8c2973404c05ebf918b19ccaf43
Anurag Kapur <anurag@beancrunch.com>
2017-07-15 22:19:45 +0100

    Remove unused code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,21 +3,13 @@ import Nav from '../components/Nav';
 import UpcomingOutings from '../components/UpcomingOutings';
 import Introduction from '../components/introduction/introduction.js'
 
-var outingSummaries = [
-  {"id":"1", "crew":"Christ Church M1", "subRequired":"cox", "date":"30 April 2017", "time":"2pm"},
-  {"id":"2", "crew":"Christ Church M3", "subRequired":"rower - strokeside", "date":"29 April 2017", "time":"2pm"},
-  {"id":"3", "crew":"Christ Church M3", "subRequired":"rower - bowside", "date":"29 April 2017", "time":"2pm"},
-  {"id":"4", "crew":"Christ Church W1", "subRequired":"cox", "date":"1 May 2017", "time":"7am"},
-  {"id":"5", "crew":"Christ Church W2", "subRequired":"cox", "date":"29 April 2017", "time":"5pm"}
-];
-
 class Home extends React.Component {
   render() {
     return (
       <div>
         <Nav />
         <Introduction/>
-        <UpcomingOutings outingSummaries={outingSummaries} />
+        <UpcomingOutings/>
       </div>
     );
   }
